29c3a8a9ff386e05e2480fe2d1d4aad45cec6ba8
# Hi, I'm Vishwa R <148326842+Dredphoenix@users.noreply.github.com>
2025-07-09 11:13:57 +0530

    ResumeCardItem.jsx

--- src/dashboard/components/ResumeCardItem.jsx
+++ src/dashboard/components/ResumeCardItem.jsx
@@ -66,7 +66,7 @@ const {resumeInfo,setResumeInfo}=useState([]);
       style={{
         borderColor:resume?.themeColor
   }}>
-       <img src="public\cv.png" alt="cv image" width={100} height={100} />
+       <img src="/cv.png" alt="cv image" width={100} height={100} />
       </div>
      
     </Link>
